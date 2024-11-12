9ef8cc799479be76c10e001c12228c882866a126
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2024-11-12 12:22:07 -0300

    feat: Add servico post and update

--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -2,8 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Servico = require("../models/servico");
 
-router.post("/", async (_req, res) => {
+router.post("/", async (req, res) => {
   try {
+    const servico = await new Servico(req.body).save();
+    res.json({ servico });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+});
+
+router.post("/:id", async (req, res) => {
+  try {
+    const { servico } = req.body;
+    await new Servico.findByIdAndUpdate(req.params.id, servico);
+    res.json({ servico });
   } catch (err) {
     res.json({ error: true, message: err.message });
   }
@@ -14,6 +26,19 @@ router.get("/", async (_req, res) => {
     const servicos = await Servico.find({ status: "A" }).select("_id titulo");
     res.json({
       servicos: servicos.map((s) => ({ label: s.titulo, value: s._id })),
+    });
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Servico.findByIdAndUpdate(id, { status: "E" });
+    res.json({
+      error: false,
     });
   } catch (err) {
     res.json({ error: true, message: err.message });
