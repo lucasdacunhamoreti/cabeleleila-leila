ac54ade2029d396e4e39a8a35c963421decd69d6
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2024-11-12 14:40:51 -0300

    feat: add pages and components clientes and agendamentos

---
+++ web/src/routes.js
@@ -0,0 +1,29 @@
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Agendamentos from "./pages/Agendamentos";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles.css";
+import Clientes from "./pages/Clientes";
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Header />
+      <div className="container-fluid h-100" style={{ backgroundColor: "red" }}>
+        <div className="row h-100">
+          <Router>
+            <Sidebar />
+
+            <Routes>
+              <Route path="/" exact Component={Agendamentos} />
+              <Route path="/clientes" Component={Clientes} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AppRoutes;
